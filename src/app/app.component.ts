faad5655ae917d5a331ed7f385b19978859330b9
Marc Brüderlin <hello@marcbruederlin.com>
2016-12-30 12:31:32 +0100

    Fixed issue #24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,7 +62,7 @@ export class MyApp {
   constructor(translate: TranslateService, platform: Platform, settings: Settings) {
     // Set the default language for translation strings, and the current language.
     translate.setDefaultLang('en');
-    translate.use('en')
+    translate.use(translate.getBrowserLang());
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
