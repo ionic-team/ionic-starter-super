c237dc213c27135867410bb6756e3ed074384319
Wilson Hobbs <wilsonhobbs1@gmail.com>
2017-01-17 18:32:21 -0500

    I did a dumb thing with promises...

--- src/providers/settings.ts
+++ src/providers/settings.ts
@@ -10,15 +10,13 @@ import { Constants } from '../app/constants';
 export class Settings {
   settings: any;
 
-  settingsObservable: Observable<any> = Observable.fromPromise(this.storage.get(Constants.STORAGE_KEY_SETTINGS)
+  load$: Observable<any> = Observable.fromPromise(this.storage.get(Constants.STORAGE_KEY_SETTINGS)
     .then((value) => {
       if (value) {
         this.settings = value;
         return this._mergeDefaults(this._defaults);
       } else {
-        return this.setAll(this._defaults).then((val) => {
-          this.settings = val;
-        })
+        return this.setAll(this._defaults)
       }
     })
   ).share();
@@ -30,73 +28,39 @@ export class Settings {
     this._defaults = defaults;
   }
 
-  _mergeDefaults(defaults: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      for (let k in defaults) {
-        if (!(k in this.settings)) {
-          this.settings[k] = defaults[k];
-        }
+  _mergeDefaults(defaults: any): void {
+    for (let k in defaults) {
+      if (!(k in this.settings)) {
+        this.settings[k] = defaults[k];
       }
-      this.setAll(this.settings).then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
-    });
+    }
+    this.setAll(this.settings);
   }
 
-  merge(settings: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      for (let k in settings) {
-        this.settings[k] = settings[k];
-      }
-      this.save().then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
+  merge(settings: any): void {
+    for (let k in settings) {
+      this.settings[k] = settings[k];
+    }
+    this.save();
+  }
+
+  setValue(key: string, value: any): void {
+    this.settings[key] = value;
+    this.storage.set(Constants.STORAGE_KEY_SETTINGS, this.settings);
+  }
+
+  setAll(value: any): void {
+    this.storage.set(Constants.STORAGE_KEY_SETTINGS, value);
+  }
+
+  getValue(key: string): any {
+    this.storage.get(Constants.STORAGE_KEY_SETTINGS).then(result => {
+      return result;
     })
   }
 
-  setValue(key: string, value: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.settings[key] = value;
-      this.storage.set(Constants.STORAGE_KEY_SETTINGS, this.settings).then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
-    })
-  }
-
-  setAll(value: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.storage.set(Constants.STORAGE_KEY_SETTINGS, value).then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
-    });
-  }
-
-  getValue(key: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.storage.get(Constants.STORAGE_KEY_SETTINGS).then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
-    });
-  }
-
-  save(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.setAll(this.settings).then(result => {
-        resolve(result);
-      }).catch(error => {
-        reject(error);
-      });
-    });
+  save(): void {
+    this.setAll(this.settings);
   }
 
   get allSettings() {
