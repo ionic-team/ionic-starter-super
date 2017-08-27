47e11dd501ea2fabeddb2136b6871e3cda70334b
Mohamed Kawsara <mkwsra@users.noreply.github.com>
2017-08-27 16:20:59 +0300

    Add dir variable

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -20,8 +20,10 @@ export interface Slide {
 export class TutorialPage {
   slides: Slide[];
   showSkip = true;
+  dir:string = 'ltr';
 
-  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService) {
+  constructor(public navCtrl: NavController, public menu: MenuController, translate: TranslateService, public platform: Platform) {
+    this.dir = platform.dir();
     translate.get(["TUTORIAL_SLIDE1_TITLE",
       "TUTORIAL_SLIDE1_DESCRIPTION",
       "TUTORIAL_SLIDE2_TITLE",
