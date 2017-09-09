c0ede5173e8b5841b6a9bdfd1ff20968a4e8c5ce
Mehdi Achour <machour@gmail.com>
2017-09-09 20:48:44 +0100

    fix(map): Comment back sample code

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -48,52 +48,52 @@ export class MapPage {
     // move the map's camera to position
     map.moveCamera(position);
 
-    create new marker
-     let markerOptions: MarkerOptions = {
-       position: ionic,
-       title: 'Ionic'
-     };
-
-     const marker: Marker = map.addMarker(markerOptions)
-       .then((marker: Marker) => {
-          marker.showInfoWindow();
-        });
-     }
-
-    initJSMaps(mapEle) {
-      new google.maps.Map(mapEle, {
-        center: { lat: 43.071584, lng: -89.380120 },
-        zoom: 16
-      });
-    }
-
-    initNativeMaps(mapEle) {
-      this.map = new GoogleMap(mapEle);
-      mapEle.classList.add('show-map');
-
-      GoogleMap.isAvailable().then(() => {
-        const position = new GoogleMapsLatLng(43.074395, -89.381056);
-        this.map.setPosition(position);
-      });
-    }
-
-    ionViewDidLoad() {
-      let mapEle = this.map.nativeElement;
-
-      if (!mapEle) {
-        console.error('Unable to initialize map, no map element with #map view reference.');
-        return;
-      }
-
-      // Disable this switch if you'd like to only use JS maps, as the APIs
-      // are slightly different between the two. However, this makes it easy
-      // to use native maps while running in Cordova, and JS maps on the web.
-      if (this.platform.is('cordova') === true) {
-        this.initNativeMaps(mapEle);
-      } else {
-        this.initJSMaps(mapEle);
-      }
-    }
+    // create new marker
+    //  let markerOptions: MarkerOptions = {
+    //    position: ionic,
+    //    title: 'Ionic'
+    //  };
+    //
+    //  const marker: Marker = map.addMarker(markerOptions)
+    //    .then((marker: Marker) => {
+    //       marker.showInfoWindow();
+    //     });
+    //  }
+    //
+    // initJSMaps(mapEle) {
+    //   new google.maps.Map(mapEle, {
+    //     center: { lat: 43.071584, lng: -89.380120 },
+    //     zoom: 16
+    //   });
+    // }
+    //
+    // initNativeMaps(mapEle) {
+    //   this.map = new GoogleMap(mapEle);
+    //   mapEle.classList.add('show-map');
+    //
+    //   GoogleMap.isAvailable().then(() => {
+    //     const position = new GoogleMapsLatLng(43.074395, -89.381056);
+    //     this.map.setPosition(position);
+    //   });
+    // }
+    //
+    // ionViewDidLoad() {
+    //   let mapEle = this.map.nativeElement;
+    //
+    //   if (!mapEle) {
+    //     console.error('Unable to initialize map, no map element with #map view reference.');
+    //     return;
+    //   }
+    //
+    //   // Disable this switch if you'd like to only use JS maps, as the APIs
+    //   // are slightly different between the two. However, this makes it easy
+    //   // to use native maps while running in Cordova, and JS maps on the web.
+    //   if (this.platform.is('cordova') === true) {
+    //     this.initNativeMaps(mapEle);
+    //   } else {
+    //     this.initJSMaps(mapEle);
+    //   }
+    // }
 
   }
 }
